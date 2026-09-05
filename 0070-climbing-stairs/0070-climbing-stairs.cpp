7808e7e6770868e547ae2ec72d1f42091730b080
AbhayThapliyal <abhaythapliyal415@gmail.com>
2026-09-05 13:41:02 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.85%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1||n==2) return n;
+
+        // vector<int> dp(n+1);
+
+        // dp[1]=1;
+        // dp[2]=2;
+
+        //memory optimization
+        int prev2=1;//n-2
+        int prev1=2;//n-1
+
+        int result=prev1;
+        
+        for(int i=3;i<=n;i++){
+            result=prev1+prev2;
+            prev2=prev1;
+            prev1=result;
+        }
+
+        return result;
+    }
+};
+
+//common recursion solution
+// int climbStairs(int n) {
+//      if(n==1 || n==2) return n;
+//          return climbStairs(n-1)+climbStairs(n-2);
+//      }
+
+
+//memoization solution
+// int helper(int n,vector<int>& dp){
+//         if(n==1 || n==2) return n;
+
+//         if(dp[n]!=-1) return dp[n];
+
+//         return dp[n]=helper(n-1,dp)+helper(n-2,dp);
+//     }
+// int climbStairs(int n) {
+//         vector<int> dp(n+1,-1);
+//         return helper(n,dp);
+//     }
